043ff4c7640f484f0e549df92dc571591fcc8c39
jordiguirao92 <jordiguirao92@users.noreply.github.com>
2020-09-04 11:34:13 +0200

    Update

--- Parte3/errorHandling.js
+++ Parte3/errorHandling.js
@@ -3,13 +3,14 @@ var fs = require('fs');
 var app = express();
 
 app.get('/', function (req, res, next) {
-    fs.readFile('/file-does-not-exist', function (err, data) {
+    fs.readFile('database.js', 'utf8', function (err, data) {
       if (err) {
         console.log('Si hay error');
         next(err); // Pass errors to Express.
       } else {
          console.log('No hay error');
          res.send(data);
+         console.log(data);
       }
     })
   })
